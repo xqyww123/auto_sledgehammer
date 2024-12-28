76f8c79e8c4622df0ecf0546d02e8b92d13602d4
Qiyuan Xu <xqyww123@gmail.com>
2024-12-28 15:49:41 +0800

    Simplified

--- Auto_Sledgehammer.thy
+++ Auto_Sledgehammer.thy
@@ -6,7 +6,6 @@ named_theorems \<phi>sledgehammer_simps \<open>Simplification rules used before 
 
 ML_file \<open>library/helpers0.ML\<close>
 ML_file \<open>library/Hasher.ML\<close>
-ML_file \<open>library/Phi_ID.ML\<close>
 ML_file \<open>library/cache_file.ML\<close>
 ML_file \<open>library/sledgehammer_solver.ML\<close>
 
