c7e562aba56266bec32d892de4b953415cb003ff
Qiyuan Xu <xqyww123@gmail.com>
2025-04-22 13:16:34 +0800

    DEBUG

--- Auto_Sledgehammer.thy
+++ Auto_Sledgehammer.thy
@@ -10,13 +10,5 @@ ML_file \<open>library/helpers0.ML\<close>
 ML_file \<open>library/Hasher.ML\<close>
 ML_file \<open>library/cache_file.ML\<close>
 ML_file \<open>library/sledgehammer_solver.ML\<close>
- 
-ML \<open> (*TEST*)
-let open Phi_Sledgehammer_Solver
-    fun assert A B = if A = B then () else error "ASSERTION"
- in assert T_ELIM  (infer_type_of_rule "conjE" @{thm conjE})
-  ; assert T_INTRO (infer_type_of_rule "conjI" @{thm conjI})
-  ; assert T_SPLIT (infer_type_of_rule "if_split_asm" @{thm if_split_asm})
-end \<close>
 
 end
